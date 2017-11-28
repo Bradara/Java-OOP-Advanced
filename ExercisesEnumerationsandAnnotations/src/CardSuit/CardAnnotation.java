3fb556972c479451abaa12042c08c44d2bb2f553
Bradara <atanas.nikolov74@gmail.com>
2017-11-28 15:23:09 +0200

    Problems for Exercises: Enumeration And Annotation

---
+++ ExercisesEnumerationsandAnnotations/src/CardSuit/CardAnnotation.java
@@ -0,0 +1,15 @@
+package CardSuit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CardAnnotation {
+    String type() default  "Enumeration";
+    String category();
+    String description();
+
+}
